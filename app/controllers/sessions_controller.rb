9dfe562fd9eb3894989df2a114574569dff26fc2
Mouhamed SECK <mseckmouhamed@gmail.com>
2023-07-17 17:37:21 +0000

    Display Notice Messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,9 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:session][:username])
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
-            flash.now[:success]="You have succesfully logged in"
-            redirect_to root_path
+            redirect_to root_path, notice: "You have succesfully logged in"
         else 
-            flash.now[:error]="THere was something wrong with your login informations"
-            render 'new'
+            render 'new', notice: "THere was something wrong with your login informations"
         end
     end
 
