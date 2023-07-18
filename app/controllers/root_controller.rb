52ea5ca97fbb02204fd6443cb1f06bfd35afe699
Mouhamed SECK <mseckmouhamed@gmail.com>
2023-07-18 10:33:36 +0000

    Add messages from UI

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,6 +2,7 @@ class RootController < ApplicationController
     before_action :require_user
     
     def index
+        @message = Message.new
         @messages = Message.all
         render 'index'
     end
